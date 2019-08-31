c8be253c2bfb61e163b301167e48802c6700b3f1
marcpre <marcus.presich@gmail.com>
2019-08-31 08:05:51 +0200

    adding form controls

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -62,4 +62,13 @@ export class RecipeEditComponent implements OnInit {
 
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    )
+  }
+
 }
