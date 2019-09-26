32736960dbc66de2f182fc9122960a242ce2741a
marcpre <marcus.presich@gmail.com>
2019-09-26 07:56:53 +0200

    adding firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    firebase.initializeApp({
+      apiKey: "AIzaSyBEOTovDg1YTEATZKwQRlZ_avVtSqA5o4Q",
+      authDomain: "ng-recipe-book-c9737.firebaseapp.com",
+    })
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
