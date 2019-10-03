6933a7e3130c1f11440ec31ec574b1b73dc7e075
marcpre <marcus.presich@gmail.com>
2019-10-03 07:49:16 +0200

    adding Auth Guard

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import * as firebase from 'firebase'
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class AuthService {
     token: string;
+
+    constructor(private router: Router) {}
 
     signupUser(email: string, password: string) {
         firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -13,9 +18,12 @@ export class AuthService {
     signinUser(email: string, password: string) {
         firebase.auth().signInWithEmailAndPassword(email, password)
         .then(
-            firebase.auth().currentUser.getToken().then(
-                (token: string) => this.token = token
-            )
+            response => {
+                this.router.navigate(['/'])
+                firebase.auth().currentUser.getToken().then(
+                    (token: string) => this.token = token
+                )
+            }
         )
         .catch(
             error  => console.log(error)
