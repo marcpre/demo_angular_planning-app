0becd0e2b694339a4b80ee31e7990fada5e48297
marcpre <marcus.presich@gmail.com>
2019-10-05 07:16:55 +0200

    refactor recipe component

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+
+import { RecipesComponent } from "./recipes.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipesStartComponent } from "./recipes-start/recipes-start.component";
+import { DropdownDirective } from "../shared/dropdown.directive";
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipesStartComponent,
+        RecipeListComponent,
+        RecipeEditComponent,
+        RecipeDetailComponent,
+        DropdownDirective
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule
+    ],
+})
+export class RecipesModule {}
